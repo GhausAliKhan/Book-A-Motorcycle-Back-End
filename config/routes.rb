090cf3d45b50504aac9b2961baec92b2095224cc
diarisdiakite <diarisdiakite@gmail.com>
2024-01-31 00:00:34 +0100

    Update routes.rb file.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create]
       resources :motorcycles, default: { format: :json }, only: [:index, :show, :create, :destroy]
+      resources :reservations, default: { format: :json }, only: [:index, :show, :create, :destroy]
       post '/login', to: 'authentication#login', default: {format: :json}
     end
   end
