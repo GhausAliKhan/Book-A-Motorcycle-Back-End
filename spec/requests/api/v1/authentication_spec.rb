1f9bce73f69da796684251fb2277fa25f78b8f60
GhausAliKhan <ghauskhan1616@gmail.com>
2024-02-01 22:16:00 +0500

    Add suggested changes

--- spec/requests/api/v1/authentication_spec.rb
+++ spec/requests/api/v1/authentication_spec.rb
@@ -12,26 +12,6 @@ RSpec.describe 'api/v1/authentication', type: :request do
         },
         required: %w[email password]
       }
-
-      # response '201', 'successful' do
-      #   let(:user) { FactoryBot.create(:user) }
-      #   let(:login_params) { { email: user.email, password: user.password } }
-      #   # let(:login_params) { { email: 'login@gmail.com', password: '123456' } }
-
-      #   after do |example|
-      #     example.metadata[:response][:content] = {
-      #       'application/json' => {
-      #         example: JSON.parse(response.body, symbolize_names: true)
-      #       }
-      #     }
-      #   end
-      #   run_test!
-      # end
-
-      # response '401', 'unauthorized' do
-      #   let(:login_params) { { email: 'nonexistent_user', password: 'invalid_password' } }
-      #   run_test!
-      # end
     end
   end
 end
